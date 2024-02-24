57b58d29d41d420f37890032a4077698cf0a2502
Sebavaldes <139097212+Sebavaldes@users.noreply.github.com>
2024-02-24 23:29:17 +0000

    commit final

--- src/models.py
+++ src/models.py
@@ -7,26 +7,49 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre_usuario = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    favoritos = relationship("Favorito")
+
+class Pokemon(Base):
+    __tablename__ = 'pokemon'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(250), nullable=False)
+    tipo = Column(String(250))
+    habilidades = relationship("Habilidad", secondary="pokemon_habilidad")
+    estadisticas = relationship("Estadistica")
 
-    def to_dict(self):
-        return {}
+class Favorito(Base):
+    __tablename__ = 'favorito'
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    pokemon_id = Column(Integer, ForeignKey('pokemon.id'))
+    usuario = relationship(Usuario)
+    pokemon = relationship(Pokemon)
+
+class Habilidad(Base):
+    __tablename__ = 'habilidad'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    descripcion = Column(String(250))
+
+class PokemonHabilidad(Base):
+    __tablename__ = 'pokemon_habilidad'
+    id = Column(Integer, primary_key=True)
+    pokemon_id = Column(Integer, ForeignKey('pokemon.id'))
+    habilidad_id = Column(Integer, ForeignKey('habilidad.id'))
+
+class Estadistica(Base):
+    __tablename__ = 'estadistica'
+    id = Column(Integer, primary_key=True)
+    pokemon_id = Column(Integer, ForeignKey('pokemon.id'))
+    tipo_estadistica = Column(String(250), nullable=False)
+    valor = Column(Integer)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
